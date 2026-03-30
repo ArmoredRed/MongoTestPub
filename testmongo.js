a57a229de619e6369eafcb537fe0a2e5c796754e
Trevor Converse <trevorconjr@gmail.com>
2026-03-30 11:35:07 -0500

    Update testmongo.js

--- testmongo.js
+++ testmongo.js
@@ -54,7 +54,7 @@ console.log("Looking for: " + searchKey);
 async function run() {
   try {
     const database = client.db('ClassMongoDB');
-    const parts = database.collection('415-Project');
+    const parts = database.collection('415-Integrated-Tech');
 
     // Hardwired Query for a part that has partID '12345'
     // const query = { partID: '12345' };
